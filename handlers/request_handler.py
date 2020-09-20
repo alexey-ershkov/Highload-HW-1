e9cf3d07de33be8860272dbb3568c4006b643be9
alexey ershkov <ershkov.alex2000@gmail.com>
2020-09-20 23:19:20 +0300

    refactoring

--- handlers/request_handler.py
+++ handlers/request_handler.py
@@ -2,15 +2,6 @@ import models
 
 
 def handle_request(connection, address, logger):
-    # logger.debug("Connected %r at %r", connection, address)
-    # while True:
-    #     data = connection.recv(1024)
-    #     if data == b"":
-    #         logger.debug("Socket closed remotely")
-    #         break
-    #     logger.debug("Received data %r", data)
-    #     connection.sendall(data)
-    #     logger.debug("Sent data")
     logger.debug("Connected %r at %r", connection, address)
     try:
         req = models.Request(connection.recv(1024))
